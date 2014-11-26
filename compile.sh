c7276e3f281a2d31df75d7971b5b3401bf6b6833
M42 <mromang08@gmail.com>
2014-11-26 14:42:13 +0100

    Translating Lambda Calculus to Haskell to Lambda Calculus

---
+++ compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# LambdaLex script.
+# Compiles Lambda calculus files into Haskell files and shows the results.
+
+INPUT=$1
+HSOUT=$INPUT.hs
+CCOUT=$INPUT.out
+
+# Messages
+echo "LambdaLex v0.1 Compiling..."
+
+# Compiling
+Lex/lambdalit $INPUT > $HSOUT
+ghc $HSOUT -o $CCOUT > /dev/null
+./$CCOUT
+
+# Cleaning
+rm $INPUT.{hi,o,hs,out}
