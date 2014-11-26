982042ece08aec22b86f00232c7a5be2212a642a
M42 <mromang08@gmail.com>
2014-11-26 11:17:58 +0100

    help.cc added Compiles Lex programs in C++. From: https://gist.github.com/fdavidcl/7ee2e6da7120ef318116

---
+++ help.cc
@@ -0,0 +1,11 @@
+/*************************************************
+Extremely helpful bit of C++ code stolen from:
+http://cs.gmu.edu/~white/CS440/usingC++.html
+(necessary for compilation)
+*************************************************/
+ 
+#include <iostream>
+#include <stdio.h>
+extern FILE *yyin;
+extern char *yytext;
+extern "C" int yylex(void); 
